6491227b553c335abbb1394d09056c2905042123
Velanora <syd.areej911@gmail.com>
2026-08-03 18:02:16 +0500

    feat : added list of tuples in project 2

--- week-2/project2.py
+++ week-2/project2.py
@@ -1,24 +1,34 @@
 option = 1
-total_expense = 0
+total_expense = 0 #global variable
+
+expenses = [] # list of tuples
 
 def add_expense():
     global total_expense
     print("================== Add new expense ================\n ")
     while True:
         try:
-            new_expense = int(input("Enter the expense amount : "))
-            total_expense += new_expense
+            category = str(input("Enter the category of expense : "))
+            amount = int(input("Enter the expense amount : "))
+            expenses.append((category, amount)) # ---> new tuple added to list
+            total_expense += amount
             break
         except ValueError:
             print("Invalid input!! please enter a number")
 
 def display():
     print("==================== Display =======================\n")
-    print("Total expense : ",total_expense)
+    if not expenses:
+        print("No expenses yet.")
+        return
+    for category, amount in expenses :
+        print(f"  *  {category} :{amount}")
+    print("\nTotal expense : ",total_expense)
 
 def exit_program():
     print("\n==================  To Exit =====================\n")
     print("Exiting......\n") 
+
 
 def invalid():
     print("====================== invalid ======================\n")
